707e841acf0a7c3849ddf3a70bbbeb8b7b960106
Chikusonu <druva186@gmail.com>
2021-07-31 13:49:20 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello worl</h1>
+      <p>functional component :)</p>
     </div>
   );
 }
