6db2d53044d154e52bb764b72e5d4cdfbbeba0e8
Chikusonu <druva186@gmail.com>
2021-07-31 13:59:34 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-
+import First from "./First"
 export default function App() {
   return (
     <div>
       <h1>Hello worl</h1>
       <p>functional component :)</p>
+      <First/>
     </div>
   );
 }
